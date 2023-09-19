6c42fc70139acb71ddf98f4be846d87601a4a9e5
vitalik <zubikvitaly@gmail.com>
2023-09-19 21:49:08 +0300

    create model tasks

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Tasks (models.Model):
+    task_name = models.CharField('Имя задачи', max_length=50)
+    task_description = models.CharField('Описание задачи', max_length=50)
+    date_create = models.DateTimeField('Дата создания задачи')
+    execution_status = models.CharField('Статус выполнения', max_length=50)
+    date_execution = models.DateTimeField('Дата выполнения задачи')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.task_name
+
+    class Meta:
+        verbose_name = 'Задачи'
+        verbose_name_plural = 'Задачи'
